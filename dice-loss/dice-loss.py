f279053d2817fb48873374167d2aa5e2c38235db
redezvous2004 <129296996+redezvous2004@users.noreply.github.com>
2026-04-11 08:03:00 +0700

    Update solution for dice-loss

--- dice-loss/dice-loss.py
+++ dice-loss/dice-loss.py
@@ -6,10 +6,7 @@ def dice_loss(p, y, eps=1e-8):
     """
     # Write code here
     p, y = map(lambda x: np.asarray(x, dtype=float), [p, y])
-    if p.ndim != 1:
-        p = p.flatten()
-    if y.ndim != 1:
-        y = y.flatten()
-    dice = (2 * np.dot(p, y) + eps) / (np.sum(p) + np.sum(y) + eps)
+
+    dice = (2 * np.sum(p * y) + eps) / (np.sum(p) + np.sum(y) + eps)
     dice_loss = 1 - dice
     return dice_loss
