1c1de1512e2e2777193945f271a84c80eed6c949
Kiên Ngô <129296996+redezvous2004@users.noreply.github.com>
2026-04-21 19:12:30 +0700

    Update solution for minmax-normalization

--- minmax-normalization/minmax-normalization.py
+++ minmax-normalization/minmax-normalization.py
@@ -7,7 +7,5 @@ def minmax_scale(X, axis=0, eps=1e-12):
     """
     # Write code here
     X = np.asarray(X)
-    if X.ndim == 1:
-        X = X[:, np.newaxis]
     norm_x = (X - np.min(X, axis=axis, keepdims=True)) / (np.max(X, axis=axis, keepdims=True) - np.min(X, axis=axis, keepdims=True) + eps)
     return norm_x
