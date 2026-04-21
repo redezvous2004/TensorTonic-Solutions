2b17d686559a790c0bebcf69b6188ac0950795f7
Kiên Ngô <129296996+redezvous2004@users.noreply.github.com>
2026-04-21 19:12:00 +0700

    Update solution for zscore-standardization

--- zscore-standardization/zscore-standardization.py
+++ zscore-standardization/zscore-standardization.py
@@ -7,8 +7,6 @@ def zscore_standardize(X, axis=0, eps=1e-12):
     """
     # Write code here
     X = np.asarray(X)
-    if X.ndim == 1:
-        X = X[:, np.newaxis]
     mean_val = np.mean(X, axis=axis, keepdims=True)
     std_val = np.std(X, axis=axis, keepdims=True)
     return (X - mean_val) / (std_val + eps)
