4d36875b45bf6c4e3d5cc817237c3bd1f98d953b
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-05 13:47:54 +0700

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -9,6 +9,6 @@ def mean_median_mode(x):
     x = np.asarray(x)
     counter = Counter(x)
     max_freq = counter.most_common(1)[0][1]
-    top_element = [v for v, k in counter.items() if k == max_freq]
-    return np.mean(x), np.median(x), min(top_element)
+    top_elements = [v for v, k in counter.items() if k == max_freq]
+    return np.mean(x), np.median(x), min(top_elements)
     
