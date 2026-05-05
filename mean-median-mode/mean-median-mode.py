c06c0c123a4f2f62e618ffe614cf65179cb1dc38
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-05 13:47:43 +0700

    Add solution for mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,14 @@
+import numpy as np
+from collections import Counter
+
+def mean_median_mode(x):
+    """
+    Compute mean, median, and mode.
+    """
+    # Write code here
+    x = np.asarray(x)
+    counter = Counter(x)
+    max_freq = counter.most_common(1)[0][1]
+    top_element = [v for v, k in counter.items() if k == max_freq]
+    return np.mean(x), np.median(x), min(top_element)
+    
