72d450eba6add29ed890c12d1317a904d2df2f9b
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-07 00:05:15 +0700

    Add solution for t-test-one-sample

---
+++ t-test-one-sample/t-test-one-sample.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def t_test_one_sample(x, mu0):
+    """
+    Compute one-sample t-statistic.
+    """
+    # Write code here
+    x = np.asarray(x)
+    mean = np.mean(x)
+    std = np.std(x, ddof=1)
+    t_stat = (mean - mu0) / (std / np.sqrt(len(x)))
+    return t_stat
