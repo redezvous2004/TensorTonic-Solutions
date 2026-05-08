c365e3719e747116650c530af52469be87d8ca41
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-08 10:03:10 +0700

    Update solution for stratified-split

--- stratified-split/stratified-split.py
+++ stratified-split/stratified-split.py
@@ -7,25 +7,23 @@ def stratified_split(X, y, test_size=0.2, rng=None):
     # Write code here
     X, y = map(lambda a: np.asarray(a), [X, y])
     if rng is None:
-        rng_to_use = np.random
-    else:
-        rng_to_use = rng
-        
+        rng = np.random
+
     train_indices = []
     test_indices = []
     classes = np.unique(y)
     for c in classes:
-        c_indices = np.where(y==c)[0]
+        c_indices = np.where(y == c)[0]
         rng.shuffle(c_indices)
         n_test = int(np.round(len(c_indices) * test_size))
         test_indices.extend(c_indices[:n_test])
         train_indices.extend(c_indices[n_test:])
     train_indices = np.sort(train_indices)
     test_indices = np.sort(test_indices)
-    
-    X_train, X_test = X[train_indices], X[test_indices]
-    y_train, y_test = y[train_indices], y[test_indices]
-    
+
+    X_train, y_train = X[train_indices], y[train_indices]
+    X_test, y_test = X[test_indices], y[test_indices]
     return X_train, X_test, y_train, y_test
+        
     
     
