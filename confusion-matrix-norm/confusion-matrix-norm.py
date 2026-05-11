c4501ecbe6b1bd5e7651e3878b0d5a3bc69cf686
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-11 14:01:39 +0700

    Update solution for confusion-matrix-norm

--- confusion-matrix-norm/confusion-matrix-norm.py
+++ confusion-matrix-norm/confusion-matrix-norm.py
@@ -8,6 +8,8 @@ def confusion_matrix_norm(y_true, y_pred, num_classes=None, normalize='none'):
     y_true, y_pred = map(lambda x: np.asarray(x), [y_true, y_pred])
     if num_classes is None:
         num_classes = max(y_true) + 1
+    if num_classes == 1:
+        return 1.0
     confusion_matrix = np.zeros((num_classes, num_classes))
     for i in range(len(y_true)):
         confusion_matrix[y_true[i]][y_pred[i]] += 1
