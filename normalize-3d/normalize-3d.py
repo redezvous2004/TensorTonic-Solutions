db274a5f206f34fccc0218cb8a46d3b448863dbd
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-20 09:54:31 +0700

    Update solution for normalize-3d

--- normalize-3d/normalize-3d.py
+++ normalize-3d/normalize-3d.py
@@ -6,6 +6,6 @@ def normalize_3d(v):
     """
     # Your code here
     v = np.asarray(v, dtype=float)
-    value_norm = np.linalg.norm(v, axis=-1, keepdims=True)
-    v_normed = np.divide(v, value_norm, out=np.zeros_like(v), where=value_norm != 0)
+    v_length = np.linalg.norm(v, axis=-1, keepdims=True)
+    v_normed = np.divide(v, v_length, out=np.zeros_like(v), where=v_length != 0)
     return v_normed
