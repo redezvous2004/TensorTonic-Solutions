e074fedd66e92444c5019a591a919532bce13fda
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-05-25 09:31:22 +0700

    Update solution for feature-store-lookup

--- feature-store-lookup/feature-store-lookup.py
+++ feature-store-lookup/feature-store-lookup.py
@@ -5,11 +5,7 @@ def feature_store_lookup(feature_store, requests, defaults):
     # Write code here
     updated_features = []
     for request in requests:
-        user_id = request["user_id"]
-        if user_id not in feature_store.keys():
-            updated_ft = {**defaults, **request["online_features"]}
-        else:
-            updated_ft = {**feature_store[user_id], **request["online_features"]}
+        updated_ft = {**feature_store.get(request["user_id"], defaults), **request["online_features"]}
         updated_features.append(updated_ft)
     return updated_features
     
