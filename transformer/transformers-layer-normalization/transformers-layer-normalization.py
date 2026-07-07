7728cc2a5aca0128e1192d5b4b8a37e26df28ad7
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-07-07 12:59:50 +0700

    Solve transformers-layer-normalization

--- transformer/transformers-layer-normalization/transformers-layer-normalization.py
+++ transformer/transformers-layer-normalization/transformers-layer-normalization.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+
 def layer_norm(x: np.ndarray, gamma: np.ndarray, beta: np.ndarray, eps: float = 1e-6) -> np.ndarray:
     """
     Returns: Normalized array of same shape as x
