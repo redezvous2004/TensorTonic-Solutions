e006a4d2dff9ad49197a8a287a78547d7fbdbddd
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-08-05 10:46:52 +0700

    Solve vae-kl-divergence

--- vae/vae-kl-divergence/vae-kl-divergence.py
+++ vae/vae-kl-divergence/vae-kl-divergence.py
@@ -5,5 +5,5 @@ def kl_divergence(mu: np.ndarray, log_var: np.ndarray) -> float:
     Returns: float scalar KL divergence averaged over the batch
     """
     # Your implementation here
-    kl_diver = - np.sum(1 + log_var - mu ** 2 - np.exp(log_var), axis=-1) / 2
-    return np.mean(kl_diver, axis=0)
+    kl_diver = -0.5 * np.mean(np.sum(1 + log_var - mu ** 2 - np.exp(log_var), axis=-1), axis=0)
+    return kl_diver
