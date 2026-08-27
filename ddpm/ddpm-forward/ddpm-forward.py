adfc7bb68406ede651dadda2502a0f5ad8871a89
Kiên Ngô <ngomanhkien2004@gmail.com>
2026-08-27 08:24:24 +0700

    Solve ddpm-forward

--- ddpm/ddpm-forward/ddpm-forward.py
+++ ddpm/ddpm-forward/ddpm-forward.py
@@ -7,7 +7,7 @@ def get_alpha_bar(betas):
     """
     # YOUR CODE HERE
     betas = np.asarray(betas, dtype=float)
-    return np.round(np.cumprod(1 - betas), 6).tolist()
+    return np.round(np.cumprod(1.0 - betas), 6)
 
 def forward_diffusion(x_0, t, betas, epsilon):
     """
@@ -16,5 +16,5 @@ def forward_diffusion(x_0, t, betas, epsilon):
     # YOUR CODE HERE
     x_0, epsilon = map(lambda a: np.asarray(a, dtype=float), [x_0, epsilon])
     alpha_bar = get_alpha_bar(betas)
-    x_t = alpha_bar[t - 1] ** 0.5 * x_0 + (1 - alpha_bar[t - 1]) ** 0.5 * epsilon
-    return x_t.tolist()
+    return  alpha_bar[t - 1] ** 0.5 * x_0 + (1 - alpha_bar[t - 1]) ** 0.5 * epsilon
+    
